a708fb79a2a241f3e55d901ceb908c76202da8ca
rc <rcastleman@gmail.com>
2017-11-23 14:08:26 -0500

    new stuff

--- Blockchain.py
+++ Blockchain.py
@@ -1,19 +1,59 @@
 # from https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
 # 23 Nov 17
 
+import hashlib
+import json
+from time import time
+
+
 class Blockchain (object):
-	"""docstring for Blockchain """
+
 	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
 
-	def new_block(self):
-		# creates new block and adds to chain
-		pass
+		# create genesis block
+		self.new_block(previous_hash = 1,proof = 100)
 
-	def new_transaction(self):
-		 # adds new transaction to the transaction llist
-		 pass
+	def new_block(self,proof,previous_hash = None):
+		"""
+		Creates new block and adds to chain
+
+		:param proof: <int> the proof given by Proof of Work algorithm
+		:param previous_hash: (Optional) <str> hash of previous block
+		:return: <dict> New Block
+
+		"""
+
+		block = {
+			'index':len(self.chain) + 1,
+			'timestamp':time(),
+			'transaction':self.current_transactions,
+			'proof':proof,
+			'previous_hash':previous_hash or self.hash(self.chain[-1]),
+		}
+
+	def new_transaction(self,sender,recipient,amount):
+		 # adds new transaction to the transaction 
+		 """
+
+		 Creates a new transaction to go into 
+		 the next mined Block 
+
+		 :param sender: <str> Address of sender
+		 :param recipient: <str> Address of recipient
+		 :param amount: <int> Amount
+		 :return: <int> index of the block that will hold this transaction 
+		 
+		 """
+		 self.current_transactions.append({
+		 	'sender':sender,
+		 	'recipient':recipient,
+		 	'amount':amount,
+		 	})
+
+		 return self.last_block['index'] + 1
+
 
 	@staticmethod
 	def hah(block):
@@ -24,4 +64,3 @@ class Blockchain (object):
 	def last_block(self):
 		# returns the last Block in the chain
 		pass
-	
